9da101060b8e3856b304c463ca75ed7eb4344897
Phil Alboroto <philalboroto@Phils-MacBook-Pro.local>
2014-09-23 20:14:00 -0600

    add changes

--- instanceArray.js
+++ instanceArray.js
@@ -12,30 +12,31 @@ var User = function(name, email, pw){
 }
 
 //Create an Array called 'users' that will store all our instances of User.
-
-  //code here
-
+    var users = [];
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
-  //code here
+    users[0] = new User('Tyler', 'tylermcginnis33@gmai.com', 'iLoveJS');
+    users[1] = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+    users[2] = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
-  //code here
+  console.log(user[0]);
 
-console.log('Lenny\'s information is ');
+console.log('Lenny\'s information is ' + user[0].name);
 //Now console.log all of Lennys information
 
-  //code here
-
+  console.log(user[2]);
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+  user[3] = new User('Phil', 'phil.alboroto@gmail.com', 'iLoveVBA');
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
 
-  //code here
+  for (var i = 0; i < users.length; i++) {
+      console.log(users[i].name);
+  }
--- quizApp.js
+++ quizApp.js
@@ -3,30 +3,41 @@
 //Let's think about the nature of this quiz app first. We're going to be creating lots of user objects, and we're
 //also going to be creating lots of Question objects. Those would make two perfectly good constructors. 
 
-//Create a User constructor that accepts name, email, password, and totalScore paramters and set them appropriatly
+//Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
-  //code here
+  var User = function(name, email, password, totalScore) {
+      this.name = name;
+      this.email= email;
+      this.password = password;
+      this.totalScore = totalScore;
+  };
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty paramters
 
-  //code here
+  var Question = function(title, answerArray, rightAnswer, difficulty) {
+      this.title = title;
+      this.answerArray = answerArray;
+      this.rightAnswer = rightAnswer;
+      this.difficulty = difficulty;
+  };
 
 
 //Create a users Array which is going to hold all of our users.
 
-  //code here
+  var users = [];
 
 
 //Let's say three people signed up for our service, create 3 intances of User and add each to the users Array
 
-  //code here
+  users[0] = new User('Phil', 'phil@gmail.com', 'phil', 0);
+  users[1] = new User('Manny', 'manny@gmail.com', 'manny', 1);
+  users[2] = new User('Michael', 'michael@gmail.com', 'michael', 2);
 
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
-
+  var questions = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -34,15 +45,19 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+  questions[0] = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?',["T", "F"], "T", 0);
+  questions[1] = new Question('T/F: JavaScript is just a scripting version of Java', ["T", "F"], "F", 1);
+  questions[2] = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value",["T", "F"], "T", 2);
 
 
-//Now push all of your instances of Question into the questions Array
-
-  //code here
-
-console.log('My users Array and my questions arrray are ...');
+console.log('My users Array and my questions array are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
+  for (var i = 0; i < users.length; i++) {
+      console.log(users[i].name)
+  }
+
+  for (var i = 0; i < questions.length; i++) {
+      console.log(questions[i].title)
+  }
 
